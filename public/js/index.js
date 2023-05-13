4c6d2262783e7bb36d9ad91a61ee8f7337cd03b5
M4RT1N <mjlynch35@yahoo.com>
2023-05-13 17:25:11 -0500

    Updated homepage

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,1 @@
-// Add an event listener to each button
-document.querySelectorAll('.navBtn').forEach(function(button) {
-    button.addEventListener('click', function(event) {
-      // Prevent the default behavior of the button
-      event.preventDefault();
-  
-      // Get the ID of the section to scroll to
-      var targetId = this.getAttribute('href');
-  
-      // Scroll to the section using smooth behavior
-      document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+
