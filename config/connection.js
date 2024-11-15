d5a4fcdf5756e23512a1be3006a4049a2d940ec6
MJ lynch <mjlynch@MJs-MacBook-Pro.local>
2024-11-14 23:52:22 -0600

    Changed timezone in connectino.js

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      timezone: '-4:00',
-    });
-    
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    timezone: '-6:00',
+  });
+
 module.exports = sequelize;
